46b77e1efcb569f5c834c47a8f144429cb841486
AlmogSkalka <almogsk14@gmail.com>
2023-01-23 13:13:09 +0200

    Exe1 Finished

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int numberToCheck = 2347623;
+            Console.WriteLine("The number to check: " +  numberToCheck);
+            Console.WriteLine("The result: " + DigitsSummer(numberToCheck));
+            Console.ReadLine();
+
+        }
+
+        static int DigitsSummer(int num)
+        {
+            if (num < 10) return num;
+            return (num % 10 + DigitsSummer(num / 10));
         }
     }
 }
