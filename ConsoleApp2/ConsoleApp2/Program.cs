e134e43cd68e78a7eb7adc7ccdaa6c6b567694e4
AlmogSkalka <almogsk14@gmail.com>
2023-01-23 14:32:37 +0200

    exe2 solved

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        int maximum = int.MaxValue;
+        int counter = 0;
+
+        int ReadNextNumber()
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                return n;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid input. Numbers only." + e);
+                return ReadNextNumber();
+            }
+        }
+
+        void ProcessStream(int nextNumber)
+        {
+            if (nextNumber == 0)
+            {
+                Console.WriteLine("Counter: "+counter);
+                return;
+            }
+
+            if (nextNumber < maximum)
+            {
+                maximum = nextNumber;
+                counter = 1;
+            }
+            else if (nextNumber == maximum)
+            {
+                counter++;
+            }
+
+            ProcessStream(ReadNextNumber());
+        }
+
+        static void Main()
+        {
+            Program prog = new Program();
+            prog.ProcessStream(prog.ReadNextNumber());
+        }
+
+    }
+}
